8f37bfd87f9a562ad8b87a25c336b660bcab7599
ealpyspa <alpyspaeva.en@gmail.com>
2025-02-13 21:42:02 +0100

    Practice 3: tasks 1-6

--- src/main/java/hw3_static_final_access_modificators/Company.java
+++ src/main/java/hw3_static_final_access_modificators/Company.java
@@ -4,33 +4,33 @@ public class Company {
     public static void main(String[] args) {
         Company employee1 = new Company(200101, "Alice Cooper");
         Company employee2 = new Company(200102, "Tom Ford");
-        Company employee3 = new Company(200103, "Julia Fox");
 
-        companyName = "Table and Chairs";
+        Company.companyName = "Table and Chairs";
 
         employee1.printCompanyName();
         employee2.printCompanyName();
-        employee3.printCompanyName();
+
     }
 
-    static String companyName;
-    final int employeeID;
-    String employeeName;
+    private static String companyName;
+    private final int employeeID;
+    private String employeeName;
 
-    Company(int employeeID, String employeeName) {
+    public Company(int employeeID, String employeeName) {
         this.employeeID = employeeID;
         this.employeeName = employeeName;
     }
 
-    static void printCompanyName() {
+    public static void printCompanyName() {
         System.out.println("Название компании: " + companyName);
     }
 
-    String getEmployeeName() {
-        return employeeName;
+    public int getEmployeeID() {
+        return employeeID;
     }
 
-    void setEmployeeName(String employeeName) {
+    public void setEmployeeName(String employeeName) {
         this.employeeName = employeeName;
     }
+
 }
