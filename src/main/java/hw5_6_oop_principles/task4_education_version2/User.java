d566032aa41aee4178634fc669c2e9bdd84a40b2
ealpyspa <alpyspaeva.en@gmail.com>
2025-03-04 21:42:23 +0100

    REFACTORED: HW 6 (oop): task 4 - education

---
+++ src/main/java/hw5_6_oop_principles/task4_education_version2/User.java
@@ -0,0 +1,19 @@
+package hw5_6_oop_principles.task4_education_version2;
+
+public abstract class User {
+    private String name;
+    private String surname;
+
+    public User(String name, String surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+}
