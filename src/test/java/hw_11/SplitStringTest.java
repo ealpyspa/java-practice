8356de8154d31fc553e24d91c68fd543a70e768e
ealpyspa <alpyspaeva.en@gmail.com>
2025-06-08 07:54:41 +0200

    HW 11: task 16

---
+++ src/test/java/hw_11/SplitStringTest.java
@@ -0,0 +1,40 @@
+package hw_11;
+
+import hw.hw_11_testing_code.SplitString;
+import hw.hw_11_testing_code.StringReverse;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SplitStringTest {
+    private SplitString splitString;
+
+    @BeforeEach
+    public void setupTest() {
+        splitString = new SplitString();
+    }
+
+    public static Stream<Arguments> stringsToBeSplitted() {
+        return Stream.of(
+                Arguments.of(
+                        "Java,Python,C++", ",", new String[] {"Java", "Python", "C++"}),
+                Arguments.of("", ",", new String[] {""}),
+                Arguments.of("word", ",", new String[]{"word"})
+        );
+    }
+
+    @ParameterizedTest
+    @MethodSource("stringsToBeSplitted")
+    public void userShouldGetSplittedString(String input, String delimiter, String[] expectedResult) {
+        String[] actualResult = splitString.splitString(input,delimiter);
+
+        assertArrayEquals(expectedResult,actualResult);
+    }
+}
